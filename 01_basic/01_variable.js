b28ab4ba34c6855b70ffed3205cbc535cbec16ab
Jahangir Alom <jahangir148a@gmail.com>
2025-05-24 20:22:28 +0000

    container configure done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = "Jahangir@123"
+let accountNumber =" 123456789"
+accountPhone =" 987654321"
+
+/*
+accountId = "2345678" "we cannot write this "
+*/
+
+accountNumber="56789876543"
+accountPhone = "9877765434567"
+
+console.log(accountId)
+
+console.table([accountId,accountPhone,accountNumber])
