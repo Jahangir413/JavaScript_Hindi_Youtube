87639c8e8262fd178b0a2096435614345c8275c0
Jahangir Alom <jahangir148a@gmail.com>
2025-05-25 12:51:52 +0000

    container configuration done

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,18 @@
+let name = "jahangir"
+let age =23
+let istheFileOpen = false 
+ 
+console.log(typeof(istheFileOpen))
+
+/* 
+number => 2 to the power 53
+bigInt => use more than 2^53 ,ussually use in trsding website ,fb,amazon
+string => "" , anything between double code i.e.""
+boolean => true /false
+nul => is a empty value not equel to zero ,null is a object 
+undefined => not defined 
+symbol = unique 
+*/
+
+console.log(typeof(null))      // object 
+console.log(typeof(undefined)) // undefined 
