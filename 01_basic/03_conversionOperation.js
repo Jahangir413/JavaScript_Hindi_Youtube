23e0a323fd960b606324d3cbe4b0919b6a6ec8df
Jahangir Alom <jahangir148a@gmail.com>
2025-05-25 13:21:00 +0000

    Conversion Operation

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let  score = "20"
+
+let scoreInString  = String(score)
+console.log(scoreInString)
+console.log(typeof scoreInString)
+
+let name = "jahangir"
+
+let nameInNumber= Number(name)
+console.log(nameInNumber)
+console.log(typeof nameInNumber)
+
+let isShopOpen ="jahangir"
+
+let bool = Boolean(isShopOpen)
+console.log(typeof bool)
+console.log( bool)
+
+// "20" => 20
+// "jahangir" => NaN
+//  1 => true ,0 => false ,"" => false, "jahangir" => true
